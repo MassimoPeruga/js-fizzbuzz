be7d16cbe448c51a3aa3ce7854e1ecb02dd0a784
MassimoPeruga <spamma-mutevole0p@icloud.com>
2023-11-08 17:07:09 +0100

    feat: Bonus 1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 'use strict';
+
+const listaFizzBuzz = document.getElementById("ms_lista-Fizz-Buzz");
+
 for (let i = 1; i <= 100; i++) {
+
+    let listItem = document.createElement("li");
+
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
+        listItem.textContent = "FizzBuzz";
     } else if (i % 3 === 0) {
-        console.log("Fizz");
+        listItem.textContent = "Fizz";
     } else if (i % 5 === 0) {
-        console.log("Buzz");
+        listItem.textContent = "Buzz";
     } else {
-        console.log(i);
+        listItem.textContent = i;
     }
+    console.log(listItem.textContent);
+    listaFizzBuzz.appendChild(listItem);
 }
